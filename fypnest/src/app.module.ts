76430317526f9de9a200cf1ca0b4214e18bfcc4f
Lotfi Arif <luffyarif58@gmail.com>
2021-10-11 01:06:06 +0800

    Dockerize nestjs and create users module/resouce

--- fypnest/src/app.module.ts
+++ fypnest/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { DatabaseConnectionService } from './database-connection.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConnectionService,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
