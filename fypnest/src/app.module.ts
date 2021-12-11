8f076a827672619c19c110d34dda23c5a1ba81a8
Lotfi Arif <Luffyarif58@gmail.com>
2021-12-11 12:05:43 +0800

    Delete app controller and service as it is unneeded

--- fypnest/src/app.module.ts
+++ fypnest/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersController } from './users/users.controller';
@@ -9,8 +7,8 @@ import { UsersService } from './users/users.service';
 
 @Module({
   imports: [PrismaModule, UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [PrismaService, AppService, UsersService],
+  controllers: [UsersController],
+  providers: [PrismaService,UsersService],
   exports: [PrismaService]
 })
 export class AppModule {}
