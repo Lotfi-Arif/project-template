dc4317a043a17b6a6da21ddaf2cb3c0e24b80fea
Lotfi Arif <Luffyarif58@gmail.com>
2022-03-19 02:08:53 +0800

    Delete extra clutter

--- fypnest/apps/fypnest/src/counsellingsessions/dto/update-counsellingsession.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCounsellingsessionDto } from './create-counsellingsession.dto';
-
-export class UpdateCounsellingsessionDto extends PartialType(CreateCounsellingsessionDto) {}
--- fypnest/apps/fypnest/src/counsellors/dto/update-counsellor.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCounsellorDto } from './create-counsellor.dto';
-
-export class UpdateCounsellorDto extends PartialType(CreateCounsellorDto) {}
