f23f2a6b15d8864a80fe6c7474793e62225b42c7
Lotfi Arif <lotfi@ninetailed.com>
2023-03-13 01:19:09 +0800

    feat: add react-redux and toolkit

--- frontend-v2/src/pages/_app.tsx
+++ frontend-v2/src/pages/_app.tsx
@@ -1,50 +1,54 @@
-import React, { type ReactElement, type ReactNode } from 'react'
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { createTheme, NextUIProvider } from '@nextui-org/react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type NextPage } from 'next'
+import React, { type ReactElement, type ReactNode } from 'react';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import store from '@/store';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type NextPage } from 'next';
 
 const lightTheme = createTheme({
   type: 'light',
   theme: {
     colors: {},
   },
-})
+});
 
 const darkTheme = createTheme({
   type: 'dark',
   theme: {
     colors: {},
   },
-})
+});
 
 export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<
   P,
   IP
 > & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout): ReactElement {
   return (
-    <NextThemesProvider
-      defaultTheme='system'
-      attribute='class'
-      value={{
-        light: lightTheme.className,
-        dark: darkTheme.className,
-      }}
-    >
-      <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>
-    </NextThemesProvider>
-  )
+    <Provider store={store}>
+      <NextThemesProvider
+        defaultTheme='system'
+        attribute='class'
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className,
+        }}
+      >
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </NextThemesProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
