515ca77d68fbd8b3be4798cda7bca9e6e6bba866
Lotfi Arif <lotfi@ninetailed.com>
2023-03-18 04:48:35 +0800

    test: add tests for courseEnrollment

--- backend/apps/fypnest/src/course-enrollment/course-enrollment.service.spec.ts
+++ backend/apps/fypnest/src/course-enrollment/course-enrollment.service.spec.ts
@@ -1,18 +1,150 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service';
 import { CourseEnrollmentService } from './course-enrollment.service';
+import { Prisma } from '@prisma/client';
 
 describe('CourseEnrollmentService', () => {
   let service: CourseEnrollmentService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CourseEnrollmentService],
+      providers: [CourseEnrollmentService, PrismaService],
     }).compile();
 
     service = module.get<CourseEnrollmentService>(CourseEnrollmentService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createCourseEnrollment', () => {
+    it('should create a course enrollment', async () => {
+      const courseEnrollmentCreateInput: Prisma.CourseEnrollmentCreateInput = {
+        course: {
+          connect: { id: 'course-id' },
+        },
+        user: {
+          connect: { id: 'user-id' },
+        },
+      };
+
+      const courseEnrollment = {
+        id: 'enrollment-id',
+        courseId: 'course-id',
+        userId: 'user-id',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      jest
+        .spyOn(prisma.courseEnrollment, 'create')
+        .mockResolvedValue(courseEnrollment as any);
+
+      const result = await service.createCourseEnrollment(
+        courseEnrollmentCreateInput,
+      );
+      expect(result).toEqual(courseEnrollment);
+    });
+  });
+  describe('getCourseEnrollmentById', () => {
+    it('should get a course enrollment by id', async () => {
+      const enrollmentId = 'enrollment-id';
+
+      const courseEnrollment = {
+        id: enrollmentId,
+        courseId: 'course-id',
+        userId: 'user-id',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      jest
+        .spyOn(prisma.courseEnrollment, 'findUnique')
+        .mockResolvedValue(courseEnrollment as any);
+
+      const result = await service.getCourseEnrollmentById(enrollmentId);
+      expect(result).toEqual(courseEnrollment);
+    });
+  });
+
+  describe('deleteCourseEnrollment', () => {
+    it('should delete a course enrollment by id', async () => {
+      const enrollmentId = 'enrollment-id';
+
+      const courseEnrollment = {
+        id: enrollmentId,
+        courseId: 'course-id',
+        userId: 'user-id',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      jest
+        .spyOn(prisma.courseEnrollment, 'delete')
+        .mockResolvedValue(courseEnrollment as any);
+
+      const result = await service.deleteCourseEnrollment(enrollmentId);
+      expect(result).toEqual(courseEnrollment);
+    });
+  });
+
+  describe('getCourseEnrollments', () => {
+    it('should get all course enrollments with pagination', async () => {
+      const courseEnrollments = [
+        {
+          id: 'enrollment-id-1',
+          courseId: 'course-id-1',
+          userId: 'user-id-1',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 'enrollment-id-2',
+          courseId: 'course-id-2',
+          userId: 'user-id-2',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+
+      jest
+        .spyOn(prisma.courseEnrollment, 'findMany')
+        .mockResolvedValue(courseEnrollments as any);
+
+      const result = await service.getCourseEnrollments({ skip: 0, take: 2 });
+      expect(result).toEqual(courseEnrollments);
+    });
+  });
+
+  describe('updateCourseEnrollment', () => {
+    it('should update a course enrollment', async () => {
+      const enrollmentId = 'enrollment-id';
+      const courseEnrollmentUpdateInput: Prisma.CourseEnrollmentUpdateInput = {
+        course: {
+          connect: { id: 'new-course-id' },
+        },
+      };
+
+      const courseEnrollment = {
+        id: enrollmentId,
+        courseId: 'new-course-id',
+        userId: 'user-id',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      jest
+        .spyOn(prisma.courseEnrollment, 'update')
+        .mockResolvedValue(courseEnrollment as any);
+
+      const result = await service.updateCourseEnrollment({
+        id: enrollmentId,
+        data: courseEnrollmentUpdateInput,
+      });
+      expect(result).toEqual(courseEnrollment);
+    });
   });
 });
