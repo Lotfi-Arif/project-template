835031d1e25084d0d6c101a7616831a99937a317
Lotfi Arif <lotfi@ninetailed.com>
2023-08-13 02:00:56 +0800

    feat: revamping frontend

--- frontend-v2/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    tsconfigPath: "tsconfig.json",
-  },
-};
-
-module.exports = nextConfig;
--- frontend-v2/src/components/styles/svg.ts
+++
@@ -1,8 +0,0 @@
-import { styled } from '@nextui-org/react';
-
-const UnstyledSvg = styled('svg', {});
-const UnstyledPath = styled('path', {});
-
-export const Svg = Object.assign(UnstyledSvg, {
-  Path: UnstyledPath,
-});
