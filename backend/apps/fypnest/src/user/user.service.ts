b604facf5f975ac4818337c52113d70279f3f853
Lotfi Arif <lotfi@ninetailed.com>
2023-11-02 02:39:35 +0700

    feat: update users module

--- backend/apps/fypnest/src/user/user.service.ts
+++ backend/apps/fypnest/src/user/user.service.ts
@@ -1,7 +1,6 @@
-// user.service.ts
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { User } from 'libs/prisma/src/generated/nestgraphql/user/user.model';
+import { User } from '@app/prisma-generated/generated/nestgraphql/user/user.model';
 import { UserCreateInput } from '@app/prisma-generated/generated/nestgraphql/user/user-create.input';
 import { UserUpdateInput } from '@app/prisma-generated/generated/nestgraphql/user/user-update.input';
 
@@ -9,23 +8,52 @@ import { UserUpdateInput } from '@app/prisma-generated/generated/nestgraphql/use
 export class UserService {
   private readonly logger = new Logger(UserService.name);
 
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
+  /**
+   * Create a new user.
+   * @param data - Data to create the user with.
+   * @returns The created user.
+   */
   async createUser(data: UserCreateInput): Promise<User> {
     this.logger.log('Creating a new user');
     return this.prisma.user.create({ data });
   }
 
+  /**
+   * Retrieve all users.
+   * @param params - Optional pagination parameters.
+   * @returns List of users.
+   */
+  async getAllUsers(
+    params: { skip?: number; take?: number } = {},
+  ): Promise<User[]> {
+    const { skip, take } = params;
+    this.logger.log(
+      `Fetching users with pagination - skip: ${skip}, take: ${take}`,
+    );
+    return this.prisma.user.findMany({ skip, take });
+  }
+
+  /**
+   * Retrieve a user by their ID.
+   * @param id - User ID.
+   * @returns The user or null if not found.
+   */
   async getUserById(id: string): Promise<User | null> {
     this.logger.log(`Fetching user by id: ${id}`);
-    return this.prisma.user.findUnique({ where: { id } });
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
   }
 
-  async getUserByEmail(email: string): Promise<User | null> {
-    this.logger.log(`Fetching user by email: ${email}`);
-    return this.prisma.user.findUnique({ where: { email } });
-  }
-
+  /**
+   * Update a user by their ID.
+   * @param params - Update parameters.
+   * @returns The updated user.
+   */
   async updateUser(params: {
     id: string;
     data: UserUpdateInput;
@@ -35,16 +63,13 @@ export class UserService {
     return this.prisma.user.update({ where: { id }, data });
   }
 
+  /**
+   * Delete a user by their ID.
+   * @param id - User ID.
+   * @returns The deleted user.
+   */
   async deleteUser(id: string): Promise<User> {
     this.logger.log(`Deleting user with id: ${id}`);
     return this.prisma.user.delete({ where: { id } });
-  }
-
-  async getUsers(params: { skip?: number; take?: number }): Promise<User[]> {
-    const { skip, take } = params;
-    this.logger.log(
-      `Fetching users with pagination - skip: ${skip}, take: ${take}`,
-    );
-    return this.prisma.user.findMany({ skip, take });
   }
 }
