78ffa2ddc09b53c1152e7d3bb3d536522c39f7d7
Lotfi Arif <lotfi@ninetailed.com>
2023-11-01 21:55:37 +0700

    feat: remove modules

--- backend/apps/fypnest/src/message/message.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MessageResolver } from './message.resolver';
-import { MessageService } from './message.service';
-
-@Module({
-  providers: [MessageResolver, MessageService],
-})
-export class MessageModule {}
--- backend/apps/fypnest/src/student/student.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StudentResolver } from './student.resolver';
-import { StudentService } from './student.service';
-import { PrismaService } from '../prisma/prisma.service';
-
-@Module({
-  providers: [StudentResolver, StudentService, PrismaService],
-})
-export class StudentModule {}
