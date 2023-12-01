c8927ebfc4e5669f2a59122b79565812533fe6ab
Lotfi Arif <lotfi@ninetailed.com>
2023-12-01 12:48:13 +0700

    Add new components and update page layout

--- frontend-v2/app/page.tsx
+++ frontend-v2/app/page.tsx
@@ -1,5 +1,23 @@
-import Image from "next/image";
+import QuickDeals from "@/components/QuickDeals";
+import RecommendedProducts from "@/components/RecommendedProducts";
+import TrendingCategories from "@/components/TrendingCategories";
+import TrendingCategoriesMain from "@/components/TrendingCategoriesMain";
+import TrendingProducts from "@/components/TrendingProducts";
+import Head from "next/head";
 
 export default function Home() {
-  return <div>something here</div>;
+  return (
+    <>
+      <Head>
+        <title>Explore Products - Framework Flow</title>
+      </Head>
+      <div>
+        <TrendingCategoriesMain />
+        <QuickDeals />
+        <TrendingProducts />
+        <RecommendedProducts />
+        <TrendingCategories />
+      </div>
+    </>
+  );
 }
