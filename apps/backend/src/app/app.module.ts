1524c398ca95a6d3ab46732d64d3f557335a2909
Lotfi Arif <lotfi@ninetailed.com>
2024-01-17 03:49:08 +0700

    feat: add OrderModule and OrderService to AppModule

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { UserModule } from '../user/user.module';
 import { PrismaService } from '../prisma/prisma.service';
 import { UserService } from '../user/user.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { OrderModule } from '../order/order.module';
+import { OrderService } from '../order/order.service';
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [UserModule, PrismaModule, OrderModule],
   controllers: [AppController],
-  providers: [AppService, UserService, PrismaService],
+  providers: [AppService, UserService, OrderService, PrismaService],
 })
 export class AppModule {}
