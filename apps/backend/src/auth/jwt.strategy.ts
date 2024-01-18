5a54741710bd7c240b6e908ec0504006fe1a44f0
Lotfi Arif <lotfi@ninetailed.com>
2024-01-19 04:57:07 +0700

    feat: add jwt constants and strategy

---
+++ apps/backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { jwtConstants } from './jwt.constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  // TODO: Add a return type, this is unfinished
+  async validate(payload: { sub: string; username: string }): Promise<unknown> {
+    return { userId: payload.sub, username: payload.username };
+  }
+}
