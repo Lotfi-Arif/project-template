ecdc34d5e57df81e6bbc8b4c4e160a9df288a909
corexaltdev <ravihidayatinisia@gmail.com>
2024-01-20 14:26:58 +0700

    feat: create unit tests for product controller

--- apps/backend/src/product/product.controller.spec.ts
+++ apps/backend/src/product/product.controller.spec.ts
@@ -1,15 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
-import { PrismaService } from '../prisma/prisma.service';
+import { mockDeep } from 'jest-mock-extended';
 
 describe('ProductController', () => {
   let controller: ProductController;
+  let service: ProductService;
 
   beforeEach(async () => {
+    service = mockDeep<ProductService>();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
-      providers: [ProductService, PrismaService],
+      providers: [{ provide: ProductService, useValue: service }],
     }).compile();
 
     controller = module.get<ProductController>(ProductController);
@@ -17,5 +19,64 @@ describe('ProductController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of products', async () => {
+      const products = [
+        {
+          id: '1',
+          name: 'test',
+          price: 100,
+          description: 'test',
+          image: 'test',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+
+      service.findAll = jest.fn().mockResolvedValue(products);
+
+      const result = await controller.findAll();
+      expect(result).toEqual(products);
+    });
+  });
+
+  describe('findOne', () => {
+    it('Should return a product', async () => {
+      const products = {
+        id: '1',
+        name: 'test',
+        price: 100,
+        description: 'test',
+        image: 'test',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      service.findOne = jest.fn().mockResolvedValue(products);
+
+      const result = await controller.findOne('1');
+      expect(result).toEqual(products);
+    });
+  });
+
+  describe('remove', () => {
+    it('Should delete a product', async () => {
+      const product = {
+        id: '1',
+        name: 'test',
+        price: 100,
+        description: 'test',
+        image: 'test',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      service.remove = jest.fn().mockResolvedValue(product);
+
+      const result = await controller.remove('1');
+      expect(result).toEqual(product);
+    });
   });
 });
