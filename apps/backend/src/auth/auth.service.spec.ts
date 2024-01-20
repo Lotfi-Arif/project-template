c71d7cb889daed39ea52379bfd906caf8f88676f
Lotfi Arif <lotfi@ninetailed.com>
2024-01-20 14:26:35 +0700

    test(auth-service): refactor auth.service.spec.ts for improved testing

--- apps/backend/src/auth/auth.service.spec.ts
+++ apps/backend/src/auth/auth.service.spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
-import { mockDeep } from 'jest-mock-extended';
+import { mock, mockDeep } from 'jest-mock-extended';
 import * as bcrypt from 'bcrypt';
+import { User } from '@tradetrove/shared-types';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -61,11 +62,7 @@ describe('AuthService', () => {
   // Additional tests for login, create, findAll, findOne, update, remove
   describe('login', () => {
     it('should return a token', async () => {
-      const user = {
-        id: '1',
-        username: 'testuser',
-        email: 'test@test.com',
-      };
+      const user = mock<Omit<User, 'password'>>();
       mockJwtService.sign = jest.fn().mockReturnValue('token');
       const result = await service.login(user);
 
@@ -76,15 +73,9 @@ describe('AuthService', () => {
     });
 
     it('should throw an error if no user is provided', async () => {
-      await expect(
-        service.login(
-          undefined as unknown as {
-            id: string;
-            username: string;
-            email: string;
-          },
-        ),
-      ).rejects.toThrow();
+      const user = null as unknown as Omit<User, 'password'>;
+      mockJwtService.sign = jest.fn().mockReturnValue('token');
+      await expect(service.login(user)).rejects.toThrow();
     });
   });
 
