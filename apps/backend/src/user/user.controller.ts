a1268f100a7f395121d8bc410fd6543ab6ef327a
Lotfi Arif <lotfi@ninetailed.com>
2024-02-08 13:15:49 +0700

    refactor: user controller and service to use result types

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -2,46 +2,127 @@ import { Controller } from '@nestjs/common';
 import { UserService } from './user.service';
 import { TypedBody, TypedParam, TypedRoute } from '@nestia/core';
 import {
-  User,
+  CreateUserResult,
+  DeleteUserResult,
+  GetAllUserResult,
+  GetUserResult,
+  UpdateUserResult,
   UserCreateInput,
   UserUpdateInput,
 } from '@tradetrove/shared-types';
+import { err, ok } from 'neverthrow';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @TypedRoute.Post()
-  create(@TypedBody() createUserDto: UserCreateInput): Promise<User> {
-    return this.userService.create(createUserDto);
+  async create(
+    @TypedBody() createUserDto: UserCreateInput,
+  ): Promise<CreateUserResult> {
+    try {
+      const user = await this.userService.create(createUserDto);
+
+      if (user.isErr()) {
+        return err(user.error);
+      }
+
+      const createdUser = user.value;
+
+      return ok(createdUser);
+    } catch (error) {
+      return err(new Error('Error creating user'));
+    }
   }
 
   @TypedRoute.Post('register')
-  async register(@TypedBody() createUserDto: UserCreateInput): Promise<User> {
-    return this.userService.create(createUserDto);
+  async register(
+    @TypedBody() createUserDto: UserCreateInput,
+  ): Promise<CreateUserResult> {
+    try {
+      const user = await this.userService.create(createUserDto);
+
+      if (user.isErr()) {
+        return err(user.error);
+      }
+
+      const createdUser = user.value;
+
+      return ok(createdUser);
+    } catch (error) {
+      return err(new Error('Error creating user'));
+    }
   }
 
   @TypedRoute.Get()
-  findAll(): Promise<User[]> {
-    return this.userService.findAll();
+  async findAll(): Promise<GetAllUserResult> {
+    try {
+      const users = await this.userService.findAll();
+
+      if (users.isErr()) {
+        return err(users.error);
+      }
+
+      const foundUsers = users.value;
+
+      return ok(foundUsers);
+    } catch (error) {
+      return err(new Error('Error finding all users'));
+    }
   }
 
   // TODO: add better error handling for not found instead of returning null
   @TypedRoute.Get(':id')
-  findOne(@TypedParam('id') id: string): Promise<User | null> {
-    return this.userService.findOne(id);
+  async findOne(@TypedParam('id') id: string): Promise<GetUserResult> {
+    try {
+      const user = await this.userService.findOne(id);
+
+      if (user.isErr()) {
+        return err(user.error);
+      }
+
+      const foundUser = user.value;
+
+      return ok(foundUser);
+    } catch (error) {
+      return err(new Error('Error finding user'));
+    }
   }
 
   @TypedRoute.Patch(':id')
-  update(
+  async update(
     @TypedParam('id') id: string,
     @TypedBody() updateUserDto: UserUpdateInput,
-  ): Promise<User> {
-    return this.userService.update(id, updateUserDto);
+  ): Promise<UpdateUserResult> {
+    try {
+      const user = await this.userService.update(id, updateUserDto);
+
+      if (user.isErr()) {
+        return err(user.error);
+      }
+
+      const updatedUser = user.value;
+
+      return ok(updatedUser);
+    } catch (error) {
+      return err(new Error('Error updating user'));
+    }
   }
 
   @TypedRoute.Delete(':id')
-  remove(@TypedParam('id') id: string): Promise<User> {
-    return this.userService.remove(id);
+  async remove(@TypedParam('id') id: string): Promise<DeleteUserResult> {
+    try {
+      const user = await this.userService.remove(id);
+
+      if (user.isErr()) {
+        return err(user.error);
+      }
+
+      const deletedUser = user.value;
+
+      return ok(deletedUser);
+    } catch (error) {
+      return err(new Error('Error removing user'));
+    }
   }
 }
--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -1,46 +1,76 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 import {
-  User,
+  CreateUserResult,
+  DeleteUserResult,
+  GetAllUserResult,
+  GetUserResult,
+  UpdateUserResult,
   UserCreateInput,
   UserUpdateInput,
 } from '@tradetrove/shared-types';
 import * as bcrypt from 'bcrypt';
+import { err, ok } from 'neverthrow';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async create(createUserDto: UserCreateInput): Promise<User> {
-    const hashedPassword = await bcrypt.hash(createUserDto.password, 10); // Hash the password
-    return this.prisma.user.create({
-      data: {
-        ...createUserDto,
-        password: hashedPassword, // Store the hashed password
-      },
-    });
+  async create(createUserDto: UserCreateInput): Promise<CreateUserResult> {
+    try {
+      const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+      // return user withoput password
+      const user = await this.prisma.user.create({
+        data: {
+          ...createUserDto,
+          password: hashedPassword,
+        },
+      });
+      return ok(user);
+    } catch (error) {
+      return err(new Error('Error creating user'));
+    }
   }
 
-  async findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
+  async findAll(): Promise<GetAllUserResult> {
+    try {
+      const users = await this.prisma.user.findMany();
+      return ok(users);
+    } catch (error) {
+      return err(new Error('Error finding all users'));
+    }
   }
 
-  async findOne(id: string): Promise<User | null> {
-    return this.prisma.user.findUnique({
-      where: { id },
-    });
+  async findOne(id: string): Promise<GetUserResult> {
+    try {
+      const user = await this.prisma.user.findUnique({ where: { id } });
+      return ok(user);
+    } catch (error) {
+      return err(new Error('Error finding user'));
+    }
   }
 
-  async update(id: string, updateUserDto: UserUpdateInput): Promise<User> {
-    return this.prisma.user.update({
-      where: { id },
-      data: updateUserDto,
-    });
+  async update(
+    id: string,
+    updateUserDto: UserUpdateInput,
+  ): Promise<UpdateUserResult> {
+    try {
+      const user = await this.prisma.user.update({
+        where: { id },
+        data: updateUserDto,
+      });
+      return ok(user);
+    } catch (error) {
+      return err(new Error('Error updating user'));
+    }
   }
 
-  async remove(id: string): Promise<User> {
-    return this.prisma.user.delete({
-      where: { id },
-    });
+  async remove(id: string): Promise<DeleteUserResult> {
+    try {
+      const user = await this.prisma.user.delete({ where: { id } });
+      return ok(user);
+    } catch (error) {
+      return err(new Error('Error removing user'));
+    }
   }
 }
