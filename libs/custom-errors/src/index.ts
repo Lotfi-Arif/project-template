c8a198c105b15b1da0677bdf85da2749fd100f22
Lotfi Arif <lotfi@ninetailed.com>
2024-05-12 21:30:53 +0800

    chore: add error-responses and error-schemas to custom-errors library

--- libs/custom-errors/src/index.ts
+++ libs/custom-errors/src/index.ts
@@ -1,10 +1,11 @@
 export * from './lib/custom-errors';
 export * from './lib/getResponseStatus';
+export * from './lib/error-responses';
+export * from './lib/error-schemas';
 export * from './lib/getResponseCode';
 export * from './lib/utils';
 
 export * from './lib/errors/http.error';
 export * from './lib/errors/jwt.error';
-export * from './lib/errors/internal.error';
 export * from './lib/errors/prisma.error';
 export * from './lib/errors/eventbus.error';
